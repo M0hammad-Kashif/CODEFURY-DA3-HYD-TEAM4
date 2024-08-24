4bfc71dcb08021e9f4053d5d3ae089c3ffa7c97c
M0hammad-Kashif <iamkashif20@gmail.com>
2024-08-24 13:19:34 +0530

    changed the return type of verify

--- src/main/java/com/hsbc/dao/CredentialDao.java
+++ src/main/java/com/hsbc/dao/CredentialDao.java
@@ -4,5 +4,5 @@ public interface CredentialDao {
     // set credentials on sign-up
     public void add(String username, String password);
     // verify credentials on sign-in
-    public void verify(String username, String password);
+    public boolean verify(String username, String password);
 }
