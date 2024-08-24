21c72595d48ec8af28b7e14efb89db1330b2179d
M0hammad-Kashif <iamkashif20@gmail.com>
2024-08-24 12:36:53 +0530

    added Parameters projectId

--- src/main/java/com/hsbc/dao/ProjectDao.java
+++ src/main/java/com/hsbc/dao/ProjectDao.java
@@ -9,13 +9,13 @@ public interface ProjectDao {
     // create a new project - returns unique project id
     public int create(String projectName, LocalDate startDate, int managerId);
     // add a team member to project
-    public void addTeamMember(int employeeId);
+    public void addTeamMember(int projectId,int employeeId);
     // remove a team member from project
-    public void removeTeamMember(int employeeId);
+    public void removeTeamMember(int projectId,int employeeId);
     // change manager of project
-    public void changeManager(int employeeId);
+    public void changeManager(int projectId,int employeeId);
     // add a bug to project
-    public void addBug(int bugId);
+    public void addBug(int projectId,int bugId);
     // find project by id
     public Project findById(int projectId);
     // find project by name
